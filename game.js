5c8eeeac0af54bd9ca640666c1e7d464440b5773
Marc Freeman <freeman.marc880@gmail.com>
2017-02-28 13:55:38 +0000

    Add files via upload

---
+++ game.js
@@ -0,0 +1,57 @@
+window.addEventListener('load', function () {
+    startGame();
+});
+
+function startGame() {
+    var canvasElement = document.getElementById('game');
+    var ctx = canvasElement.getContext('2d');
+    var squares = [
+        {
+            y: 0,
+            x: 0,
+            color: "#000"
+        },
+        {
+            y: 60,
+            x: 20,
+            color: "#ffb900"
+        },
+        {
+            y: 120,
+            x: 0,
+            color: "#000"
+        },
+        {
+            y: 180,
+            x: -20,
+            color: "#f00"
+        }
+    ];
+
+
+    var x = 0;
+
+    setInterval(updateFrame, 200);
+
+    function updateFrame() {
+        x++;
+        clearScreen(ctx, canvasElement);
+
+        for (var i = 0; i < squares.length; i++) {
+            var square = squares[i];
+            drawSquare(ctx, square.x + x, square.y, square.color);
+        }
+    }
+}
+
+
+function drawSquare(ctx, x, y, color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, 50, 50);
+}
+
+
+function clearScreen(ctx, canvasElement) {
+    ctx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+}
+
